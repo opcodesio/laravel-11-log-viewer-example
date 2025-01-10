e5db7f3aa0f690302226738fc730b1f5960d0ef8
Arunas Skirius <arukomp@gmail.com>
2025-01-10 10:18:13 +0200

    add link to Log Viewer in main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,13 @@
                                     </a>
                                 @else
                                     <a
+                                        href="/log-viewer"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Log Viewer
+                                    </a>
+
+                                    <a
                                         href="{{ route('login') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
